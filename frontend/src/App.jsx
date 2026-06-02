4e9fdca293e93a9a8acfc33db34cc4a0b50a5f5a
Joseph-375 <josephcse1@gmail.com>
2026-06-02 15:38:48 +0530

    first page of commit message

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginScreen from './components/LoginScreen';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <LoginScreen />
+    </div>
+  );
+}
+
+export default App;
